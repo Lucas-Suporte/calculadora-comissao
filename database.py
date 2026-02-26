c0b9218edce65577b33c8dc38983dcaafbc2b40f
Lucas-Suporte <lucas.araujo@pet247market.com.br>
2026-02-26 15:14:45 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,21 @@
+import sqlite3
 
+def conectar():
+    return sqlite3.connect("database.db", check_same_thread=False)
+
+def criar_tabela_usuarios():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        senha TEXT NOT NULL,
+        perfil TEXT NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
